cb5cd3df644f61185ea36b29f34e047bcd834225
Ian Candy <iancandysemail@gmail.com>
2019-10-21 15:52:16 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,5 +2,6 @@
 def meal_choice(vegetable, starch, protein="meat")
   meal = "A plate of #{protein} with #{vegetable} and #{starch}."
   puts "What a nutritious meal!"
-  p meal
+  puts meal
+  meal
 end
